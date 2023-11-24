55fb0ed0666e2d3f083a73ec8eead8967a7f7c37
Rubel Mahmud <vxlrubel@gmail.com>
2023-11-24 14:41:15 +0600

    update script

--- assets/js/admin/admin-script.js
+++ assets/js/admin/admin-script.js
@@ -10,12 +10,24 @@
             AddNew.on('click', function(e){
                 e.preventDefault();
                 let selfTextStore = $(this).text();
+                let noticeSuccess = '<div class="wsf-notice"><span>Successfully add a new subscriber...</span><span class="wsf-notice-dismiss">&times;</span></div>';
                 $(this).empty().html('<span class="wsf-ajax-loading"></span>');
 
                 setTimeout(()=>{
                     $(this).empty().text(selfTextStore);
+                    $(this).closest('.wrap').children('.wsf-title').after(noticeSuccess)
                 },1000);
-            })
+            });
+
+            $('.add-new-subscribe-form-parent').on('click', function(e){
+                console.dir(e.target.className);
+                if( e.target.className == 'wsf-notice-dismiss' ){
+                    $(this).find('.wsf-notice').fadeOut(400);
+                    setTimeout(() => {
+                        $(this).find('.wsf-notice').remove();
+                    }, 500);
+                }
+            });
         }
     }
 
--- inc/add-new-subscriber-form.php
+++ inc/add-new-subscriber-form.php
@@ -4,18 +4,16 @@
 defined('ABSPATH') || exit;
 ?>
 
-<div class="wrap">
+<div class="wrap add-new-subscribe-form-parent">
     <div class="wsf-title">
         <h2>Add New Subscriber</h2>
         <a href="<?php echo admin_url( "admin.php?page={$this->slug}" );?>">View Subscriber</a>
     </div>
-    <div class="wsf-notice">
-        Successfully add a new subscriber...
-    </div>
+    
     <form action="" class="wsf-add-new-subscriber-form">
         <div class="label-parent">
             <input type="email" placeholder="Enter Email Address">
-            <button type="button" id="add_new_subscriber"><span class="wsf-ajax-loading"></span></button>
+            <button type="button" id="add_new_subscriber">Add New</button>
         </div>
     </form>
 </div>
